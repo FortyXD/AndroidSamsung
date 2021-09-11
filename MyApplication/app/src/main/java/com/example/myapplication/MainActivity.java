e536b87dfec9b56cc5005fa45fb6aa5d7521b4cb
andrey <mais06062005@gmail.com>
2021-09-11 10:29:19 +0300

    /

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,46 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
 
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     button1 =(Button) findViewById(R.id.button);
     button1.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
             openActivity2();
         }
     });
@@ -29,5 +50,4 @@ public void openActivity2(){
         startActivity(intent);
 
 }
-
 }
